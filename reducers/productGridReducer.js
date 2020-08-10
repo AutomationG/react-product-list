62f15f1d271862ed54cc219ae34942deaa31040f
AutomationG <pareek23gargi@gmail.com>
2020-08-11 02:40:07 +0530

    2 files modified

--- reducers/productGridReducer.js
+++ reducers/productGridReducer.js
@@ -5,7 +5,7 @@ export const productList = (state = initialState, action) => {
   switch (action.type) {
     case "ADD_DATA":
       console.log('reducer')
-      console.log(state)
+      console.log(action)
       return {...state,productData:action.payload}
       // return Object.assign({}, state, {productData: 
       //   state.productData.concat(action.payload) });
