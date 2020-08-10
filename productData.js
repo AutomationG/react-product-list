4efd33e0c94b4e49a080c21b77846ee48dab3181
AutomationG <pareek23gargi@gmail.com>
2020-08-10 14:06:47 +0530

    1 file added, 1 deleted

---
+++ productData.js
@@ -0,0 +1,30 @@
+const ProductData =
+[
+    {
+        src :'src\\assest\\images\\AKS PEACH KURTI.jpg',
+        brand:'AKS',
+        product:'AKS Peach Kurti',
+        price:'Rs 900'
+    },
+    {
+        src :'src\\assest\\images\\AURELIA KURTA.jpg',
+        brand:'AURELIA',
+        product:'Pink kurti',
+        price:'Rs 1100'
+    },
+    {
+        src :'src\\assest\\images\\JOMPERS KURTA.jpg',
+        brand:'JOMPERS',
+        product:'Pink Kurti',
+        price:'Rs 800'
+    },
+    {
+        src :'src\\assest\\images\\KHUSAL KURTA.jpg',
+        brand:'KHUSAL',
+        product:'Peach Kurti',
+        price:'Rs 900'
+    }
+
+]
+
+export default ProductData;
